8e16e362cce90cd114a3bf40e917de4c62704bf8
suvotheking007 <suvotheking007@gmail.com>
2022-05-07 01:34:25 +0530

    application can now communicate with remote mongodb database

---
+++ jobhunters/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  DB_URL: `mongodb+srv://suvo007:hXr665CZrvjqpiCr@cluster0.i5zbj.mongodb.net/jobhunters`,
+};
--- jobhunters/server.js
+++ jobhunters/server.js
@@ -3,7 +3,21 @@ const Express = require("express");
 // Server requirements
 const { PORT: appPort } = require("./config/server.config");
 
+// MongoDB requirements
+const { DB_URL } = require("./config/db.config");
+
+// Mongoose requirements
+const Mongoose = require("mongoose");
+
 const app = Express();
+
+Mongoose.connect(DB_URL, (err) => {
+  if (err) {
+    return console.log("error in connection", err);
+  }
+
+  return console.log("Connection successful");
+});
 
 module.exports = app.listen(appPort, () => {
   console.log(`app has been started on port number ${appPort}`);
