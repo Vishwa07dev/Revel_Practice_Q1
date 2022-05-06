069d7dc25a611839837e190e93f5851aa5396b10
suvotheking007 <suvotheking007@gmail.com>
2022-05-07 02:09:38 +0530

    application is now able to recieve request in json format and send response in js object

--- jobhunters/server.js
+++ jobhunters/server.js
@@ -2,6 +2,7 @@ const Express = require("express");
 
 // Server requirements
 const { PORT: appPort } = require("./config/server.config");
+const bodyParser = require("body-parser");
 
 // MongoDB requirements
 const { DB_URL } = require("./config/db.config");
@@ -11,12 +12,19 @@ const Mongoose = require("mongoose");
 
 const app = Express();
 
+app.use(bodyParser.json()); // Essential to convert the incoming json request body to a js object
+app.use(bodyParser.urlencoded({ extended: true }));
+
 Mongoose.connect(DB_URL, (err) => {
   if (err) {
     return console.log("error in connection", err);
   }
 
   return console.log("Connection successful");
+});
+
+app.get("/", (req, res) => {
+  return res.status(200).send({ requestBody: req.body });
 });
 
 module.exports = app.listen(appPort, () => {
