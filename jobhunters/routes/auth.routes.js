860129cba00bcc1f76dc0e3e74cc85e2967cee52
Uday <budayteja009@gmail.com>
2022-05-07 17:34:01 +0530

    Added codes

--- jobhunters/routes/auth.routes.js
+++ jobhunters/routes/auth.routes.js
@@ -6,4 +6,6 @@ module.exports = (app) => {
     app.post("/jobhunters/api/v1/auth/signup",  authController.signup);
 
     app.post("/jobhunters/api/v1/auth/signin",  authController.signin);
+
+    app.post("/jobhunters/api/v1/compaines", companyController.createCompany);
 }
