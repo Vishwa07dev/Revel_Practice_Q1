80e7268168e6fddf3ab08597f8453c8730e2adfa
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-05-07 15:55:37 +0530

    steps for getting the latest update

--- jobhunters/Story1.js
+++ jobhunters/Story1.js
@@ -7,10 +7,41 @@
  * 
  * 
  * 
- *      POST :   /jobhunter/api/v1/signin  - login
+ *      POST :   /jobhunter/api/v1/signin  
  * 
  * 
  *      Return me the JWT access token
+ * 
+ * 
+ * 1. Fork
+ * 2. Pull the latest code from Vishwa main branch
+ * 3. Create a new local branch called as auth
+ * 4. Make the changes to support authentication and authorization
+ * 5. Push to your repo
+ * 6. Raise the PR
+ * 
+ * 
+ * To get the latest code from Vishwa repo
+ * 
+ * 1. Need to add vishwa repo to your local code base
+ * 
+ * git remote add  upstream < Vishwa code repo>
+ * 
+ * git fetch upstream  // this will get you latest changes done by vishwa
+ * 
+ * create a new branch based on upstream/main
+ * 
+ * git checkout -b auth upstream/main
+ * 
+ * 
+ * -- make changes
+ * 
+ * git push -u origin auth
+ * 
+ * 
+ * // Raise the PR from git hub 
+ * 
+ * 
  * 
  * 
  * 
