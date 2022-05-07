692943784bf3361cc74e1d8f03eb608924aec010
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-05-07 17:34:07 +0530

    code

--- jobhunters/app.js
+++ jobhunters/app.js
@@ -16,29 +16,28 @@ require("./routes")(app);
 /**
  * Setup the mongodb connection and create on ADMIN user
  */
- mongoose.connect(dbConfig.DB_URL, async() => {
+mongoose.connect(dbConfig.DB_URL, async () => {
     console.log("MongoDB connected");
-    
+
 
 
     await User.collection.drop();// Since this a dev setup
-    
-    // if(user == null){
-        user = await User.create({
-            name : "Vishwa Mohan",
-            userId : "admin",
-            password : bcrypt.hashSync("Welcome1",8),
-            email : "kankvish@gmail.com",
-            userType :  constant.userType.admin   
-        });
-        console.log("admin created", user);
-    // }
-    
+
+    const user = await User.create({
+        name: "Vishwa Mohan",
+        userId: "admin",
+        password: bcrypt.hashSync("Welcome1", 8),
+        email: "kankvish@gmail.com",
+        userType: constant.userType.admin
+    });
+    console.log("admin created", user);
+
+
 })
 
 /**
  * Start the express server
  */
- app.listen(serverConfig.PORT, () => {
+app.listen(serverConfig.PORT, () => {
     console.log("Application has started on the port ", serverConfig.PORT);
 })
