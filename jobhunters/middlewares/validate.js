ae2345d62ec206d87d05622ea7e0b2fa61f46831
suvotheking007 <suvotheking007@gmail.com>
2022-05-07 14:00:09 +0530

    Implemented checking for correct email structure

--- jobhunters/middlewares/validate.js
+++ jobhunters/middlewares/validate.js
@@ -1,6 +1,9 @@
 // Constants requirements
 const { httpCodes, userTypes } = require("../constants");
 
+// Utility requirements
+const Check = require("../utils/check");
+
 // Signup request validator
 exports.signUp = (req, res, next) => {
   // Validations for mandatory fields
@@ -25,12 +28,23 @@ exports.signUp = (req, res, next) => {
     });
   }
 
-  // Validations for correctness of the provided field values
+  // Validations for correctness of the provided field values -------------------------------
+
+  //   Check for correct role value
   if (req.params.role && !userTypes.includes(req.params.role)) {
     return res.status(httpCodes.badRequest).send({
       message: "provided role value is invalid",
     });
   }
+
+  //   Check for correct email format
+  if (!Check.email(req.body.email)) {
+    return res.status(httpCodes.badRequest).send({
+      message: "Email format is invalid",
+    });
+  }
+
+  // --------------------------------------------------------------------------------------------
 
   // Pass control to next function
   next();
