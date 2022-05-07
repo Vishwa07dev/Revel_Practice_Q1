df85af40841a415ee27ca93f679e17a5f848949b
suvotheking007 <suvotheking007@gmail.com>
2022-05-07 16:32:13 +0530

    Small mistake fixed

--- jobhunters/routes/user.routes.js
+++ jobhunters/routes/user.routes.js
@@ -2,7 +2,7 @@
 const Auth = require("../controllers/auth.controller");
 
 module.exports = (app) => {
-  app.post("/jobhunters/api/vi/signup/:roles", Auth.signUp);
+  app.post("/jobhunters/api/vi/signup/:role", Auth.signUp);
   app.post("/jobhunters/api/vi/signup/", Auth.signUp);
   app.post("/jobhunters/api/vi/login/", Auth.login);
 };
