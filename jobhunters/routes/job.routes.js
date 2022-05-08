daf8d11094759ffe638d391158573252f291e9b3
Masthan <masthan55591@gmail.com>
2022-05-08 11:26:01 +0530

    minor changes

--- jobhunters/routes/job.routes.js
+++ jobhunters/routes/job.routes.js
@@ -10,18 +10,18 @@ const {auth} = require("../middlewares/");
 module.exports = (app)=>{
     
     // CREATE CALL
-    app.post("/jobhunters/app/v1/companies", [auth.verifyToken, auth.isAdminOrRecruiter], jobController.createJob);
+    app.post("/jobhunters/app/v1/jobs", [auth.verifyToken, auth.isAdminOrRecruiter], jobController.createJob);
 
     // UPDATE CALL
-    app.put("/jobhunters/app/v1/companies/:id", [auth.verifyToken], jobController.updateJob);
+    app.put("/jobhunters/app/v1/jobs/:id", [auth.verifyToken], jobController.updateJob);
 
     // DELETE CALL
-    app.delete("/jobhunters/app/v1/companies/:id", [auth.verifyToken, auth.isAdminOrRecruiter], jobController.deleteJob);
+    app.delete("/jobhunters/app/v1/jobs/:id", [auth.verifyToken, auth.isAdminOrRecruiter], jobController.deleteJob);
     
     // GET ALL COMPANIES
-    app.get("/jobhunters/app/v1/companies", [auth.verifyToken], jobController.getAllJobs);
+    app.get("/jobhunters/app/v1/jobs", [auth.verifyToken], jobController.getAllJobs);
     
     // GET SINGLE COMPANY
-    app.get("/jobhunters/app/v1/companies/:id", [auth.verifyToken], jobController.getOneJob);
+    app.get("/jobhunters/app/v1/jobs/:id", [auth.verifyToken], jobController.getOneJob);
 
 }
