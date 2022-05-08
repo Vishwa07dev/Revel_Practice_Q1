4a01e71cd599b5929e1e1401f42c89c37830c5f0
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-05-08 10:55:21 +0530

    code for company APIs

--- jobhunters/routes/company.routes.js
+++ jobhunters/routes/company.routes.js
@@ -10,18 +10,18 @@ const {auth} = require("../middlewares/");
 module.exports = (app)=>{
     
     // CREATE CALL
-    app.post("/jobhunter/ap/v1/companies", [auth.verifyToken, auth.isAdminOrRecruiter], companyController.addCompany);
+    app.post("/jobhunters/app/v1/companies", [auth.verifyToken, auth.isAdminOrRecruiter], companyController.addCompany);
 
     // UPDATE CALL
-    app.put("/jobhunter/ap/v1/companies/:id", [auth.verifyToken, auth.isAdminOrRecruiter], companyController.updateCompany);
+    app.put("/jobhunters/app/v1/companies/:id", [auth.verifyToken, auth.isAdminOrRecruiter], companyController.updateCompany);
 
     // DELETE CALL
-    app.delete("/jobhunter/ap/v1/companies/:id", [auth.verifyToken, auth.isAdminOrRecruiter], companyController.deleteCompany);
+    app.delete("/jobhunters/app/v1/companies/:id", [auth.verifyToken, auth.isAdminOrRecruiter], companyController.deleteCompany);
     
     // GET ALL COMPANIES
-    app.get("/jobhunter/ap/v1/companies", [auth.verifyToken], companyController.getAllCompanies);
+    app.get("/jobhunters/app/v1/companies", [auth.verifyToken], companyController.getAllCompanies);
     
     // GET SINGLE COMPANY
-    app.get("/jobhunter/ap/v1/companies/:id", [auth.verifyToken], companyController.getOneCompany);
+    app.get("/jobhunters/app/v1/companies/:id", [auth.verifyToken], companyController.getOneCompany);
 
 }
